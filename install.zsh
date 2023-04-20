0cb18bf1b85e75d917563c56423bda9824e2e057
Arthur Meltonyan <42479733+arthurmeltonyan@users.noreply.github.com>
2023-04-20 13:35:09 +0400

    Update install.zsh

--- install.zsh
+++ install.zsh
@@ -21,6 +21,7 @@ brew install --cask brave-browser
 brew install --cask google-chrome
 brew install --cask firefox
 brew install --cask opera
+brew install --cask arc
 brew install --cask transmission
 brew install --cask zoom
 brew install --cask telegram
@@ -67,9 +68,6 @@ brew install neovim
 git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1
 LV_BRANCH='release-1.2/neovim-0.8' 
 curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/fc6873809934917b470bff1b072171879899a36b/utils/installer/install.sh
-
-
-
 
 
 curl --proto "=https" --tlsv1.2 -sSf https://sh.rustup.rs | sh
