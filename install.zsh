5c49be9375e95e59a648e3ad6b1e6deb3ec9f112
Arthur Meltonyan <42479733+arthurmeltonyan@users.noreply.github.com>
2023-04-20 13:26:11 +0400

    Update install.zsh

--- install.zsh
+++ install.zsh
@@ -64,7 +64,12 @@ git config --global user.email arthur.meltonyan@gmail.com
 
 
 brew install neovim
-git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1 && nvim
+git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1
+LV_BRANCH='release-1.2/neovim-0.8' 
+curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/fc6873809934917b470bff1b072171879899a36b/utils/installer/install.sh
+
+
+
 
 
 curl --proto "=https" --tlsv1.2 -sSf https://sh.rustup.rs | sh
