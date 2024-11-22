fe4dc2a4b289f01478bb9b6344bf2d072d20ed8a
A M <42479733+arthurmeltonyan@users.noreply.github.com>
2024-11-22 15:12:02 -0800

    Update install.zsh

--- install.zsh
+++ install.zsh
@@ -4,9 +4,5 @@ git config --global user.name "Arthur Meltonyan"
 git config --global user.email arthur.meltonyan@gmail.com
 
 
-pyenv install 3.10
-pyenv global 3.10
-
-
 pip3 install --user --upgrade pip
 curl -sSL https://install.python-poetry.org | python3 -
